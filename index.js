93765cc21fd841fa1481480b143d104a1b967225
hanfeng5 <xhfeng5@uw.edu>
2023-02-20 17:31:32 -0800

    added moving and repeating feature

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ let GROUND_SPRITE_HEIGHT = 60
 let numGroundSprites
 let GRAVITY
 let player
+let pressedKeys = {}
+let speed = 3
+let score
 
-window.setup = () => {
-    createCanvas(windowWidth, windowHeight);
-    //   spr = createSprite(width / 2, height / 2, width /3, height/3);
-    //   spr.shapeColor = color(255)
-    GRAVITY = 1;
+function createEnemy () {
+  setInterval(() => {
     groupSprites = new Group()
 
-    numGroundSprites = windowWidth / GROUND_SPRITE_WIDTH -10;
+  numGroundSprites = windowWidth / GROUND_SPRITE_WIDTH -20;
 
     for (var n = 0; n < numGroundSprites; n++) {
         var groundSprite1 = createSprite(
@@ -42,29 +42,86 @@ window.setup = () => {
         groupSprites.add(groundSprite1);
         groupSprites.add(groundSprite2);
     }
+  }, 8000);
+}
+  
+
+
+window.setup = () => {
+    score = 0;
+    createCanvas(windowWidth, windowHeight);
+    GRAVITY = 1;
+    setInterval(createEnemy(),1000);
     player = createSprite(windowWidth/2, windowHeight-100, 30, 30)
 }
 
-window.draw = () => {
-  background(60);
-  drawSprites()
+
+
+
+window.update = () => {
+  let movement = createVector(0,0);
+
+  if (keyIsPressed) {
+    if (pressedKeys[UP_ARROW]){
+      movement.y -= 1;
+    }
+    if (pressedKeys[DOWN_ARROW]) {
+      movement.y += 1;
+    }
+    if (pressedKeys[LEFT_ARROW]) {
+      movement.x -= 1;
+    }
+    if (pressedKeys[RIGHT_ARROW]) {
+      movement.x += 1;
+    }
+  }
+  movement.setMag(speed);
+  player.x += movement.x;
+  player.y += movement.y;
+
 }
 
- window.keyPressed = () => {
-    if (keyCode == RIGHT_ARROW) {
-      player.setSpeed(1.5, 0);
-    }
-    else if (keyCode == DOWN_ARROW) {
-      player.setSpeed(1.5, 90);
-    }
-    else if (keyCode == LEFT_ARROW) {
-      player.setSpeed(1.5, 180);
-    }
-    else if (keyCode == UP_ARROW) {
-      player.setSpeed(1.5, 270);
-    }
-    else if (key == ' ') {
-      player.setSpeed(0, 0);
-    }
-    return false;
-  }
+
+window.draw = () => {
+  background(60);
+  drawSprites();
+  update();
+  score = score+1
+  textAlign(CENTER);
+  text(score, camera.position.x, 50);
+  // playerMove()
+}
+
+window.keyReleased = () => {
+  delete pressedKeys[keyCode];
+}
+
+window.keyPressed = () => {
+  pressedKeys[keyCode] = true;
+}
+
+
+
+
+//     // if (keyCode == RIGHT_ARROW) {
+//     //   // player.setSpeed(1.5, 360);
+//     //   player.x += 20
+//     // }
+//     // else if (keyCode == DOWN_ARROW) {
+//     //   player.setSpeed(1.5, 90);
+//     //   player.x += 0.2
+//     // }
+//     // else if (keyCode == LEFT_ARROW) {
+//     //   player.setSpeed(1.5, 180);
+//     //   player.x += 0.2
+//     // }
+//     // else if (keyCode == UP_ARROW) {
+//     //   player.setSpeed(1.5, 270);
+//     //   player.x += 0.2
+//     // }
+//     // else if (key == ' ') {
+//     //   player.setSpeed(0, 0);
+//     //   player.x += 0.2
+//     // }
+//     // return false;
+//   }
